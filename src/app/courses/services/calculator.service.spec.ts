80364f4b6b8507cf4a2c6bcbcc74889baf853dbc
AndreyUA <infernalsteel666@gmail.com>
2022-12-22 16:48:29 +0200

    spyOn for tracking and toHaveBeenCalledTimes method

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,11 +3,19 @@ import { LoggerService } from "./logger.service";
 
 describe("CalculatorService", () => {
   it("should add two numbers", () => {
-    const calculator = new CalculatorService(new LoggerService());
+    const logger = new LoggerService();
+
+    // Track this instance and replace method
+    // it will help to detect how many times
+    // we are using this method(s)
+    spyOn(logger, "log");
+
+    const calculator = new CalculatorService(logger);
 
     const result = calculator.add(2, 4);
 
     expect(result).toBe(6);
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
 
   it("should subtract two numbers", () => {
