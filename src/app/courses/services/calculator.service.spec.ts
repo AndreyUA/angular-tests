c3a13fc7bb03814dd346bd862120d3030972546f
AndreyUA <infernalsteel666@gmail.com>
2022-12-22 16:30:18 +0200

    implementation of add and subtract tests functions

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,12 +1,23 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
 describe("CalculatorService", () => {
   it("should add two numbers", () => {
-    pending();
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.add(2, 4);
+
+    expect(result).toBe(6);
   });
 
   it("should subtract two numbers", () => {
     // Mock uncompleted tests with pending()
     // Mock failed tests with fail()
-    
-    fail();
+
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.subtract(4, 2);
+
+    expect(result).toBe(2, "unexpected subtract error");
   });
 });
