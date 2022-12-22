5f7192628d57743f3582baf28aa35bfdd10bf944
AndreyUA <infernalsteel666@gmail.com>
2022-12-22 16:01:46 +0200

    created first spec file, mock tests with pending and fail

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,12 @@
+describe("CalculatorService", () => {
+  it("should add two numbers", () => {
+    pending();
+  });
+
+  it("should subtract two numbers", () => {
+    // Mock uncompleted tests with pending()
+    // Mock failed tests with fail()
+    
+    fail();
+  });
+});
