ade861a3fb4693401e44487ca5f38700cb0bb7db
AndreyUA <infernalsteel666@gmail.com>
2022-12-25 21:02:17 +0200

    beforeEach for executing something before every test case

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -2,33 +2,26 @@ import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 
 describe("CalculatorService", () => {
+  let calculator: CalculatorService;
+  let loggerSpy: any;
+
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
+
+    calculator = new CalculatorService(loggerSpy);
+  });
+
   it("should add two numbers", () => {
-    // const logger = new LoggerService();
-
-    // Track this instance and replace method
-    // it will help to detect how many times
-    // we are using this method(s)
-    // spyOn(logger, "log");
-
-    // Create fake implementation of LoggerService
-    const logger = jasmine.createSpyObj("LoggerService", ["log"]);
-
-    const calculator = new CalculatorService(logger);
-
     const result = calculator.add(2, 4);
 
     expect(result).toBe(6);
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it("should subtract two numbers", () => {
-    // Mock uncompleted tests with pending()
-    // Mock failed tests with fail()
-
-    const calculator = new CalculatorService(new LoggerService());
-
     const result = calculator.subtract(4, 2);
 
     expect(result).toBe(2, "unexpected subtract error");
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 });
