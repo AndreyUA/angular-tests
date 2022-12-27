6af1ff76270c3ef824ca8ea6c8923f4ded41dc93
AndreyUA <infernalsteel666@gmail.com>
2022-12-27 10:30:48 +0200

    basic configuration for test for CoursesCardListComponent

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -9,6 +9,12 @@ import { Course } from "../model/course";
 import { setupCourses } from "../common/setup-test-data";
 
 describe("CoursesCardListComponent", () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [CoursesModule],
+    });
+  });
+
   it("should create the component", () => {
     pending();
   });
