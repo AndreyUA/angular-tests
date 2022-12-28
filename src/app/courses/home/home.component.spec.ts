2597b753fbced902cbc13c7d520985f1a3bd89f9
AndreyUA <infernalsteel666@gmail.com>
2022-12-28 16:51:10 +0200

    should display only beginner courses case

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -4,11 +4,19 @@ import { DebugElement } from "@angular/core";
 import { HomeComponent } from "./home.component";
 import { CoursesService } from "../services/courses.service";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { setupCourses } from "../common/setup-test-data";
+import { By } from "@angular/platform-browser";
+import { of } from "rxjs";
 
 describe("HomeComponent", () => {
   let fixture: ComponentFixture<HomeComponent>;
   let component: HomeComponent;
   let element: DebugElement;
+  let courseService: any;
+
+  const beginnerCourses = setupCourses().filter(
+    (course) => course.category === "BEGINNER"
+  );
 
   beforeEach(waitForAsync(() => {
     const coursesServiceSpy = jasmine.createSpyObj("CoursesService", [
@@ -29,6 +37,7 @@ describe("HomeComponent", () => {
         fixture = TestBed.createComponent(HomeComponent);
         component = fixture.componentInstance;
         element = fixture.debugElement;
+        courseService = TestBed.inject(CoursesService);
       });
   }));
 
@@ -37,7 +46,12 @@ describe("HomeComponent", () => {
   });
 
   it("should display only beginner courses", () => {
-    pending();
+    courseService.findAllCourses.and.returnValue(of(beginnerCourses));
+    fixture.detectChanges();
+
+    const tabs = element.queryAll(By.css(".mdc-tab"));
+
+    expect(tabs.length).withContext("Unexpected numbers of tabs found").toBe(1);
   });
 
   it("should display only advanced courses", () => {
