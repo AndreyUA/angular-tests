4fa103e665f0a6e3268bdb86f3b08d4a5168e472
AndreyUA <infernalsteel666@gmail.com>
2022-12-31 17:07:32 +0200

    fakeAsync with tick callback

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,3 +1,5 @@
+import { fakeAsync, tick } from "@angular/core/testing";
+
 fdescribe("async testing examples", () => {
   it("async test example with done callback by Jasmine", function (done: DoneFn) {
     let test: boolean = false;
@@ -15,4 +17,16 @@ fdescribe("async testing examples", () => {
       done();
     }, 1000);
   });
+
+  it("async test example with setTimeout and without done callback", fakeAsync(function () {
+    let test: boolean = false;
+
+    setTimeout(() => {
+      test = true;
+    }, 1000);
+
+    tick(1000);
+
+    expect(test).toBe(true);
+  }));
 });
